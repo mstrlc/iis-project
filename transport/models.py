a13408853f172d3d5391de7d443fa947e69fd3d8
Maxmilián Nový <xnovym00@stud.fit.vutbr.cz>
2023-11-23 23:38:18 +0100

    Added models

--- transport/models.py
+++ transport/models.py
@@ -17,6 +17,13 @@ from sqlalchemy import (
 from transport.extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+line_stops = Table('LineStop',
+                    Column('line_id', Integer, ForeignKey('lines.id'), primary_key = True),
+                    Column('stop_id', Integer, ForeignKey('stops.id'), primary_key = True),
+                    Column('time_from_start', DateTime, nullable=False),
+                    Column('order', Integer, nullable=False)
+                    )
+
 class User(db.Model):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -56,3 +63,42 @@ class User(db.Model):
         db.session.add(self)
         db.session.commit()
         db.session.expunge_all()
+
+class Stop(db.model):
+    __tablename__='stops'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    latitude = Column(FLOAT, nullable=False)
+    longitude = Column(FLOAT, nullable=False)
+    
+class Line(db.model):
+    __tablename__='lines'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    connections = relationship('Connection', backref='lines')
+    line_stops = relationship('Stop', secondary=line_stops, backref='lines')
+    
+class Vehicle(db.model):
+    __tablename__='vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    type = Column(String(100), nullable=False)
+    make = Column(String(100), nullable=False)
+    model = Column(String(100), nullable=False)
+    specs = Column(String(150), nullable=False)
+    connections = relationship('Connection', backref='vehicles')
+    
+class Connection(db.model):
+    __tablename__='connections'
+    id = Column(Integer, primary_key=True)
+    time = Column(DateTime, nullable=False)
+    direction = Column(String(20), nullable=False)
+    days_of_week = Column(String(100), nullable=False)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'), nullable=True)
+    line_id = Column(Integer, ForeignKey('lines.id'), nullable=False)
+
+class Maintenance(db.model):
+    __tablename__='maintenance'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, nullable=False)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'), nullable=True)
